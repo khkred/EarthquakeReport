83d2579578259e26b9f490df4fe56c81d717d546
khkred <khkrlife@gmail.com>
2021-02-07 19:54:41 +0530

    Json Added in String Format

--- app/src/main/java/com/fsemicolon/earthquakereport/MainActivity.java
+++ app/src/main/java/com/fsemicolon/earthquakereport/MainActivity.java
@@ -29,23 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        ArrayList<Earthquake> earthquakesList = new ArrayList<>();
-
-
-        earthquakesList.add(new Earthquake(7.2,"San Fransisco","Feb 2 2018"));
-
-        earthquakesList.add(new Earthquake(6.1,"London","Sep 25 2018"));
-
-        earthquakesList.add(new Earthquake(8.9,"Tokyo","Feb 14 2020"));
-
-        earthquakesList.add(new Earthquake(9.6,"Paris","March 1 2015"));
-
-        earthquakesList.add(new Earthquake(3.2,"Warsaw","June 12 2018"));
-
-        earthquakesList.add(new Earthquake(2.7,"Delhi","Sep 10 2015"));
-
-        earthquakesList.add(new Earthquake(9.5,"Berlin","April 2 2021"));
-
+        ArrayList<Earthquake> earthquakesList = QueryUtils.extractEarthquakes();
 
         //find the id for recyclerView
 
