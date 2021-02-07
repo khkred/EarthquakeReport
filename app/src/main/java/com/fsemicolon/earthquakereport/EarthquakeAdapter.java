f1c69ae5d9361eb36dd93dccc460e6ccd5480c86
khkred <khkrlife@gmail.com>
2021-02-07 14:48:25 +0530

    Everything Added.

--- app/src/main/java/com/fsemicolon/earthquakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/fsemicolon/earthquakereport/EarthquakeAdapter.java
@@ -1,11 +1,71 @@
 package com.fsemicolon.earthquakereport;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.loader.app.LoaderManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class EarthquakeAdapter  {
+import java.util.ArrayList;
+
+/**
+ * STEP 2: EXTEND THE ADAPTER CLASS IN OUR EarthquakeAdapter
+ */
+
+public class EarthquakeAdapter extends RecyclerView.Adapter<EarthquakeAdapter.EarthquakeViewHolder> {
+
+
+    private ArrayList<String> mPlaces;
+
+
+    //We need to create a constructor in order to get the cities from our Main Activity
+
+    public EarthquakeAdapter(ArrayList<String> places)
+    {
+        mPlaces = places;
+    }
+
+    //Here are we going to implement the onCreateViewHolder method
+
+
+    @NonNull
+    @Override
+    public EarthquakeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        //First step is to get the context
+
+        Context context = parent.getContext();
+
+        //Next step is to call the Layout Inflater for getting the view
+
+        View itemView = LayoutInflater.from(context)
+                .inflate(R.layout.list_item,parent,false);
+
+        //Finally we have to return an EarthquakeViewHolder
+
+        return new EarthquakeViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EarthquakeViewHolder holder, int position) {
+
+
+
+        holder.bind(position);
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+        int size =mPlaces.size();
+        return
+                mPlaces.size();
+    }
 
     /**
      * STEP 1: DEFINE A VIEW HOLDER CLASS
@@ -22,13 +82,14 @@ public class EarthquakeAdapter  {
             placeTextView = itemView.findViewById(R.id.location_text_view);
         }
 
-        /**
-         * Let's define a bind method in order to add the data to our placeTextView
-         */
+        //Let's define a bind method in order to add the data to our placeTextView
 
-        public void bind(String place)
+        public void bind(int position)
         {
-            placeTextView.setText(place);
+            String city = mPlaces.get(position);
+
+            placeTextView.setText(city);
+
         }
     }
 
--- app/src/main/java/com/fsemicolon/earthquakereport/MainActivity.java
+++ app/src/main/java/com/fsemicolon/earthquakereport/MainActivity.java
@@ -1,12 +1,23 @@
 package com.fsemicolon.earthquakereport;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * STEP 3: CREATE A LINEAR LAYOUT MANAGER IN ORDER TO WIRE UP THE ENTIRE RECYCLERVIEW
+     */
+
+    private RecyclerView mRecyclerView;
+
+    private EarthquakeAdapter mEarthquakeAdapter;
+
 
     ArrayList<String> cities;
 
@@ -15,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cities = new ArrayList<>();
+        cities = new ArrayList<String>();
 
         cities.add("San Fransisco");
         cities.add("London");
@@ -24,5 +35,49 @@ public class MainActivity extends AppCompatActivity {
         cities.add("Warsaw");
         cities.add("Delhi");
         cities.add("Berlin");
+
+        //find the id for recyclerView
+
+        mRecyclerView = findViewById(R.id.recycler_view);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+
+        // Connect layoutManager to our recyclerView
+        mRecyclerView.setLayoutManager(layoutManager);
+
+
+        //Instantiate our Adapter
+        mEarthquakeAdapter = new EarthquakeAdapter(cities);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        // Connect the recyclerView to the Adapter
+
+
+        mRecyclerView.setAdapter(mEarthquakeAdapter);
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
