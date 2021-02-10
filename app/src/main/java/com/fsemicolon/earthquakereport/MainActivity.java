1255203ca9e284c049a6fc7ae650fc7ce2fc8b49
khkred <khkrlife@gmail.com>
2021-02-10 18:23:06 +0530

    Added the AsyncTask

--- app/src/main/java/com/fsemicolon/earthquakereport/MainActivity.java
+++ app/src/main/java/com/fsemicolon/earthquakereport/MainActivity.java
@@ -6,9 +6,14 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
 
+import java.io.IOException;
 import java.lang.reflect.Array;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements EarthquakeAdapter.ListItemClickListener {
@@ -25,6 +30,9 @@ public class MainActivity extends AppCompatActivity implements EarthquakeAdapter
 
     ArrayList<Earthquake> earthquakesList;
 
+    private static final String USGS_EARTHQUAKE_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=6&limit=10";
+
+
     ArrayList<String> cities;
 
     @Override
@@ -33,32 +41,13 @@ public class MainActivity extends AppCompatActivity implements EarthquakeAdapter
         setContentView(R.layout.activity_main);
 
 
-
-        earthquakesList = QueryUtils.extractEarthquakes();
+        new EarthquakeAsyncTask().execute(USGS_EARTHQUAKE_URL);
 
         //find the id for recyclerView
 
         mRecyclerView = findViewById(R.id.recycler_view);
 
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
 
-        // Connect layoutManager to our recyclerView
-        mRecyclerView.setLayoutManager(layoutManager);
-
-
-        //Instantiate our Adapter
-        mEarthquakeAdapter = new EarthquakeAdapter(earthquakesList,this);
-
-        mRecyclerView.setHasFixedSize(true);
-
-        // Connect the recyclerView to the Adapter
-
-
-        mRecyclerView.setAdapter(mEarthquakeAdapter);
-
-        //We are setting the type of our intent
-
-        mWebPageIntent = new Intent(Intent.ACTION_VIEW);
 
 
     }
@@ -75,8 +64,72 @@ public class MainActivity extends AppCompatActivity implements EarthquakeAdapter
 
         startActivity(mWebPageIntent);
 
+    }
+
+    public void updateUI(String jsonResponse)
+    {
+
+        earthquakesList = QueryUtils.extractEarthquakes(jsonResponse);
+        //Instantiate our Adapter
+        mEarthquakeAdapter = new EarthquakeAdapter(earthquakesList,this);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+
+        // Connect layoutManager to our recyclerView
+        mRecyclerView.setLayoutManager(layoutManager);
 
 
+
+        mRecyclerView.setHasFixedSize(true);
+
+        // Connect the recyclerView to the Adapter
+
+
+        mRecyclerView.setAdapter(mEarthquakeAdapter);
+
+        //We are setting the type of our intent
+
+        mWebPageIntent = new Intent(Intent.ACTION_VIEW);
+
+    }
+
+    public class EarthquakeAsyncTask extends AsyncTask<String,Void,String>
+    {
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String urlString = urls[0];
+
+            if (urlString!=null && !(TextUtils.isEmpty(urlString)))
+            {
+                try {
+                    URL url = new URL(urlString);
+
+                    String jsonResponse = QueryUtils.makeHttpRequest(url);
+
+                    return jsonResponse;
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String jsonResponse) {
+
+            if (jsonResponse!=null&&!(TextUtils.isEmpty(jsonResponse)))
+            {
+                updateUI(jsonResponse);
+
+
+            }
+        }
     }
 }
 
